003a34c41f737c701f67eed99613b928815986d1
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-21 22:38:27 +0530

    Added Fibonacci series file File

---
+++ Day-6-P1-Fibonaci/Day-6-P1-Fibonaci/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter the Number");
+double num = Convert.ToDouble(Console.ReadLine());
+int sum = 0;
+Console.WriteLine("Series are as follows");
+for (int i = 0; i <= num; i++)
+{
+    int[] number = { i };
+    sum += i;
+    Console.WriteLine(sum);
+}
