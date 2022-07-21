9aea7b3d4e6018d06ffecca7780e064a4f1fd525
SudhanshuYadav1998 <sy7040@gmail.com>
2022-07-21 22:50:07 +0530

    Added StopWatch File

---
+++ Day-6-StopWatch/Day-6-StopWatch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        // Create new stopwatch from class Stopwatch
+        Stopwatch stopwatch = new Stopwatch();
+
+        // here we start the timing for stopwatch
+        stopwatch.Start();
+
+        // calculating time elapsed for 1000 sec
+        for (int i = 0; i < 1000; i++)
+        {
+            Thread.Sleep(1);
+        }
+
+        // Stop the counting of stopwatch
+        stopwatch.Stop();
+
+        // Output the result 
+        Console.WriteLine($"Time elapsed:  {stopwatch.Elapsed}");
+    }
+}
